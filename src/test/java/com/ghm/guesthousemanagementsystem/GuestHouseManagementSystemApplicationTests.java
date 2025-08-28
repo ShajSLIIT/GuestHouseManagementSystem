bb4ed61655fc54a4fa1cd2375b975daa76cd275b
ShajSLIIT <it24610789@my.sliit.lk>
2025-08-28 14:44:18 +0530

    Initial commit

---
+++ src/test/java/com/ghm/guesthousemanagementsystem/GuestHouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghm.guesthousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestHouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
